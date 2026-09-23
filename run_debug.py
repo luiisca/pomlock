3d8d4c20c75dcada2a971ae3d2d4da60380759ff
Luis Cadillo <luiscaf3r@gmail.com>
2026-09-22 21:50:43 -0500

    chore: remove unneeded files

--- run_debug.py
+++
@@ -1,7 +0,0 @@
-import sys
-# Replace the timer_engine module with our debug version
-sys.modules['pomlock.timer_engine'] = __import__('pomlock.timer_engine_debug')
-# Now import the app
-from pomlock.ui.app import PomlockApp
-app = PomlockApp()
-app.run()
